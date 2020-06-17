8c4ac07f08c1dfed0e68f5ac3586c7c488419f59
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-17 23:14:27 +0200

    add player animation

--- Space Hero/Assets/Scripts/PlayerMovementController.cs
+++ Space Hero/Assets/Scripts/PlayerMovementController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     public float Speed = 10.0f;
-    new Rigidbody2D rigidbody;
+    Rigidbody2D playerRigidbody;
+    Animator playerAnimator;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Fixed count of calls per second.
@@ -21,6 +23,10 @@ public class PlayerMovementController : MonoBehaviour
         Vector2 direction = new Vector2(horizontal, vertical);
 
         // Normalized for length one in each directions
-        rigidbody.velocity = direction.normalized * Speed;
+        playerRigidbody.velocity = direction.normalized * Speed;
+
+        playerAnimator.SetBool("IsFlyingLeft", horizontal < 0);
+        playerAnimator.SetBool("IsFlyingRight", horizontal > 0);
+        playerAnimator.SetBool("IsFlyingUp", vertical > 0);
     }
 }
