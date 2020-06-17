609a83934169c967dd2a1f8e8c451d055b168c8f
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-17 22:39:01 +0200

    add player movement

---
+++ Space Hero/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    new Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Fixed count of calls per second.
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+
+        // Normalized for length one in each directions
+        rigidbody.velocity = direction.normalized * Speed;
+    }
+}
