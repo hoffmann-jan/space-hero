dd1f2b9c2acfc6bb712cd8c18ea69a982dfe84bc
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-22 22:02:20 +0200

    add spawn points

---
+++ Space Hero/Assets/Scripts/SpawnMonstersController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMonstersController : MonoBehaviour
+{
+    public GameObject Monster1;
+    public GameObject Monster2;
+    public float Interval = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnMonster), Interval, Interval);
+    }
+
+    private void SpawnMonster()
+    {
+        float random = Random.value;
+
+        if (random <= 0.2)
+        {
+            Instantiate(Monster1, transform.position, Quaternion.identity);
+        }
+        else if (random <= 0.4)
+        {
+            Instantiate(Monster2, transform.position, Quaternion.identity);
+        }
+    }
+}
